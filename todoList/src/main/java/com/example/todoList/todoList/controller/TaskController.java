6d00dd379c7851ea72347c05725eacb2aea7c3ee
gui.2899 <gui.2899>
2025-06-04 17:05:38 -0300

    Adicionando a anotação PostMapping na função create

--- todoList/src/main/java/com/example/todoList/todoList/controller/TaskController.java
+++ todoList/src/main/java/com/example/todoList/todoList/controller/TaskController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @PostMapping
     public ResponseEntity<Void> createTask(@RequestBody @Valid TaskRequestDTO taskRequestDTO) {
         taskService.saveTask(taskRequestDTO);
         return ResponseEntity.ok().build();
