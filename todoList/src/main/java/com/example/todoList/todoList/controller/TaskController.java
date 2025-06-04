2cfa1631e4b9ed0233bc01f5fcc7462be2341190
gui.2899 <gui.2899>
2025-06-04 17:27:58 -0300

    Adicionando documentação a nossa classe controller

--- todoList/src/main/java/com/example/todoList/todoList/controller/TaskController.java
+++ todoList/src/main/java/com/example/todoList/todoList/controller/TaskController.java
@@ -2,9 +2,13 @@ package com.example.todoList.todoList.controller;
 
 import com.example.todoList.todoList.dto.TaskRequestDTO;
 import com.example.todoList.todoList.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,10 +23,15 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Operation(summary = "Create a new Task", description = "save new task in database with initial status PENDING")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "created task successfully"),
+            @ApiResponse(responseCode = "400", description = "invalid request")
+    })
     @PostMapping
     public ResponseEntity<Void> createTask(@RequestBody @Valid TaskRequestDTO taskRequestDTO) {
         taskService.saveTask(taskRequestDTO);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
