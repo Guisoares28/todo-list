53495205258b53d3179925b8becf9f1d40171789
gui.2899 <gui.2899>
2025-06-04 16:47:16 -0300

    Criação da classe Mapper, do bean e suas funções de conversão

--- todoList/src/main/java/com/example/todoList/todoList/config/Mapper.java
+++ todoList/src/main/java/com/example/todoList/todoList/config/Mapper.java
@@ -1,13 +1,38 @@
 package com.example.todoList.todoList.config;
-
-
+import com.example.todoList.todoList.dto.TaskRequestDTO;
+import com.example.todoList.todoList.dto.TaskResponseDTO;
 import com.example.todoList.todoList.model.TaskEntity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class Mapper {
 
+    public TaskEntity toEntity(TaskRequestDTO dto) {
+        return new TaskEntity(
+                dto.description(),
+                dto.finalDate(),
+                dto.current()
+        );
+    }
+
+    public TaskResponseDTO toResponse(TaskEntity task) {
+        return new TaskResponseDTO(
+                task.getDescription(),
+                task.getFinalDate(),
+                task.getCurrent(),
+                task.getCreatedDate(),
+                task.getStatus()
+        );
+    }
+
+    public List<TaskResponseDTO> toListResponse(List<TaskEntity> taskEntities){
+        return taskEntities.stream()
+                .map(this::toResponse)
+                .toList();
+    }
 
     @Bean
     public Mapper mapper() {
