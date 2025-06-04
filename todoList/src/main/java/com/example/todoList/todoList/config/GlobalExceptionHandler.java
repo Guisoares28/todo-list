1d2ed0d06de8b1c8db761a68e2b7be1ea82329ff
gui.2899 <gui.2899>
2025-06-04 17:26:56 -0300

    Criação da classe Exception Handler, para gerenciar nossos erros e retornar status concissos

---
+++ todoList/src/main/java/com/example/todoList/todoList/config/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.todoList.todoList.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + " : " + error.getDefaultMessage())
+                .toList();
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
