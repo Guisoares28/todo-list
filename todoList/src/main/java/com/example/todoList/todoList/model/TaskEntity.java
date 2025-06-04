86746fb8b8bbde7c6f23581c4162e049684a2569
gui.2899 <gui.2899>
2025-06-04 17:36:56 -0300

    Adicionando a tag Schema para documentar nossa API

--- todoList/src/main/java/com/example/todoList/todoList/model/TaskEntity.java
+++ todoList/src/main/java/com/example/todoList/todoList/model/TaskEntity.java
@@ -2,6 +2,7 @@ package com.example.todoList.todoList.model;
 
 
 import com.example.todoList.todoList.enums.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,15 +21,21 @@ public class TaskEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Schema(description = "task description", example = "pay electricity bill")
     private String description;
 
+    @Schema(description = "task created date, This value is default when constructing an instance", example = "04/06/2025")
     private LocalDate createdDate;
 
+    @Schema(description = "task final date, This value represents the final date to perform the task", example = "20/07/2025")
     private LocalDate finalDate;
 
+    @Schema(description = "The status represents the period in which our task is located. this value is default" +
+            "when constructing as instance", example = "PEDING")
     @Enumerated(EnumType.STRING)
     private Status status;
 
+    @Schema(description = "recurring represents a bill that needs to be paid every month", example = "true or false")
     private Boolean current;
 
     public TaskEntity(String description, LocalDate finalDate, Boolean current) {
