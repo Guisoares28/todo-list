c4af2d96ec6890704d285471957a09efb41b9db2
gui.2899 <gui.2899>
2025-06-05 17:04:56 -0300

    Criando as querys para task

--- todoList/src/main/java/com/example/todoList/todoList/repository/TaskRepository.java
+++ todoList/src/main/java/com/example/todoList/todoList/repository/TaskRepository.java
@@ -5,7 +5,9 @@ import com.example.todoList.todoList.model.TaskEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -15,7 +17,10 @@ public interface TaskRepository extends ListPagingAndSortingRepository<TaskEntit
 
     Page<TaskEntity> findAll(Pageable pageable);
 
-    Page<TaskEntity> findAByFinalDateBetween(Pageable pageable, LocalDate startDate, LocalDate endDate);
+    @Query("SELECT t FROM TaskEntity t WHERE t.finalDate BETWEEN :start AND :end")
+    Page<TaskEntity> findByFinalDateBetween(Pageable pageable,
+                                             @Param("start") LocalDate startDate,
+                                             @Param("end") LocalDate endDate);
 
     Page<TaskEntity> findByStatus(Pageable pageable, Status status);
 
