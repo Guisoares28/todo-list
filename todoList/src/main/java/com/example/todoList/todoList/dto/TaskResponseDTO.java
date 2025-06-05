853befbdc2e21e21a53a228827fab2ae0ff24ac1
gui.2899 <gui.2899>
2025-06-05 17:03:23 -0300

    Adicionando as tags do Swagger schema

--- todoList/src/main/java/com/example/todoList/todoList/dto/TaskResponseDTO.java
+++ todoList/src/main/java/com/example/todoList/todoList/dto/TaskResponseDTO.java
@@ -1,13 +1,16 @@
 package com.example.todoList.todoList.dto;
 
 import com.example.todoList.todoList.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDate;
 
 public record TaskResponseDTO(
         String description,
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate finalDate,
         Boolean current,
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate createDate,
         Status status
 ) {
