9f0d5334305a02431a8e8cecaf8d00661e4c3764
gui.2899 <gui.2899>
2025-06-05 12:18:53 -0300

    Criação das funções de cadastrar e buscar tarefas

--- todoList/src/main/java/com/example/todoList/todoList/controller/TaskController.java
+++ todoList/src/main/java/com/example/todoList/todoList/controller/TaskController.java
@@ -1,7 +1,9 @@
 package com.example.todoList.todoList.controller;
 
 import com.example.todoList.todoList.dto.TaskRequestDTO;
+import com.example.todoList.todoList.dto.TaskResponseDTO;
 import com.example.todoList.todoList.model.TaskEntity;
+import com.example.todoList.todoList.service.CrudService;
 import com.example.todoList.todoList.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -10,9 +12,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
@@ -20,7 +26,7 @@ import org.springframework.web.bind.annotation.*;
 public class TaskController {
 
     @Autowired
-    private TaskService taskService;
+    private CrudService<TaskEntity, Long> taskService;
 
     @Operation(summary = "Create a new Task", description = "save new task in database with initial status PENDING")
     @ApiResponses(value = {
@@ -28,17 +34,34 @@ public class TaskController {
             @ApiResponse(responseCode = "400", description = "invalid request")
     })
     @PostMapping
-    public ResponseEntity<Void> createTask(@RequestBody @Valid TaskRequestDTO taskRequestDTO) {
-        taskService.saveTask(taskRequestDTO);
+    public ResponseEntity<Void> saveTasks(@RequestBody @Valid TaskRequestDTO taskRequestDTO) {
+        taskService.create(taskRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Get task for id", description = "Get task for Id, case id not exists then return NotFoundException")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get Task for id Successfully" ),
+            @ApiResponse(responseCode = "404", description = "Task id not found")
+    })
+    @GetMapping("/forId/{id}")
+    public ResponseEntity<TaskResponseDTO> getTaskForId(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(taskService.findById(id));
+    }
+
+
+    @Operation(summary = "Get all tasks for page", description = "Get all tasks, \n" +
+            "informing the page in the url and size page")
     @GetMapping
-    public ResponseEntity<Page<TaskEntity>> getAllTasks(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
-    ){
-        return ResponseEntity.ok().body(taskService.getAllTasks(page,size));
+    public ResponseEntity<List<TaskResponseDTO>> getAllTasks(Pageable pageable) {
+        return ResponseEntity.ok().body(taskService.findAll(pageable).getContent());
+    }
+
+    @GetMapping("/dates")
+    public ResponseEntity<List<TaskResponseDTO>> getAllTasksForBetWeenDates(@RequestParam(value = "startDate")LocalDate startDate,
+                                                                            @RequestParam(value = "endDate")LocalDate endDate,
+                                                                            Pageable pageable) {
+        return ResponseEntity.ok().body(taskService.findByFinalDate(pageable, startDate, endDate).getContent());
     }
 
 }
