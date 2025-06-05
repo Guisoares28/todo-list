346ad1e31d2aadba41d0ab4edde3c557ba8a6425
gui.2899 <gui.2899>
2025-06-05 17:07:04 -0300

    Criação dos serviços de consulta, atualização e deleção

--- todoList/src/main/java/com/example/todoList/todoList/service/TaskService.java
+++ todoList/src/main/java/com/example/todoList/todoList/service/TaskService.java
@@ -3,6 +3,7 @@ package com.example.todoList.todoList.service;
 import com.example.todoList.todoList.config.Mapper;
 import com.example.todoList.todoList.dto.TaskRequestDTO;
 import com.example.todoList.todoList.dto.TaskResponseDTO;
+import com.example.todoList.todoList.dto.UpdateRequestDTO;
 import com.example.todoList.todoList.enums.Status;
 import com.example.todoList.todoList.exception.NotFoundException;
 import com.example.todoList.todoList.model.TaskEntity;
@@ -43,16 +44,65 @@ public class TaskService implements CrudService<TaskEntity, Long> {
 
     @Override
     public Page<TaskResponseDTO> findByFinalDate(Pageable pageable, LocalDate startDate, LocalDate endDate) {
-        return Mapper.toPageResponse(taskRepository.findAByFinalDateBetween(pageable, startDate ,endDate));
+        Page<TaskResponseDTO> content = Mapper.toPageResponse(taskRepository.findByFinalDateBetween(pageable, startDate ,endDate));
+        if(content.isEmpty()){
+            throw new NotFoundException("Tasks not found for the given range");
+        }
+        return content;
     }
 
     @Override
     public Page<TaskResponseDTO> findByStatus(Pageable pageable, Status status) {
-        return Mapper.toPageResponse(taskRepository.findByStatus(pageable, status));
+        Page<TaskResponseDTO> content = Mapper.toPageResponse(taskRepository.findByStatus(pageable, status));
+        if(content.isEmpty()){
+            throw new NotFoundException("Tasks not found for the given status");
+        }
+        return content;
     }
 
     @Override
     public Page<TaskResponseDTO> findByCurrent(Pageable pageable, Boolean current) {
-        return Mapper.toPageResponse(taskRepository.findByCurrent(pageable, current));
+        Page<TaskResponseDTO> content = Mapper.toPageResponse(taskRepository.findByCurrent(pageable, current));
+        if(content.isEmpty()){
+            throw new NotFoundException("Tasks not found for the given current");
+        }
+        return content;
+    }
+
+    @Override
+    public void update(Long id, UpdateRequestDTO updateDTO) {
+        TaskEntity task = taskRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Task not found with " + id));
+        if(updateDTO.description() != null && !updateDTO.description().isEmpty()){
+            task.setDescription(updateDTO.description());
+        }
+        if(updateDTO.finalDate() != null && !updateDTO.finalDate().isBefore(LocalDate.now())){
+            task.setFinalDate(updateDTO.finalDate());
+        }
+        taskRepository.save(task);
+    }
+
+    @Override
+    public void updateStatus(Long id, Status status) {
+        TaskEntity task = taskRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Not found task for id" + id));
+        task.setStatus(status);
+        taskRepository.save(task);
+    }
+
+    @Override
+    public void updateCurrent(Long id, Boolean current) {
+        TaskEntity task = taskRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Not found task for id" + id));
+        task.setCurrent(current);
+        taskRepository.save(task);
+    }
+
+    @Override
+    public void deleteTask(Long id) {
+        if(!taskRepository.existsById(id)){
+            throw new NotFoundException("Not found task for id" + id);
+        }
+        taskRepository.deleteById(id);
     }
 }
