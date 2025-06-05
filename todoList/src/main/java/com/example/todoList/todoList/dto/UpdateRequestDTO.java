e45ecdd86ca4473321fd27b88a1ead730b351a7d
gui.2899 <gui.2899>
2025-06-05 17:03:48 -0300

    Adicionando as tags schema do swagger

--- todoList/src/main/java/com/example/todoList/todoList/dto/UpdateRequestDTO.java
+++ todoList/src/main/java/com/example/todoList/todoList/dto/UpdateRequestDTO.java
@@ -1,4 +1,18 @@
 package com.example.todoList.todoList.dto;
 
-public record UpdateRequestDTO() {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.time.LocalDate;
+
+public record UpdateRequestDTO(
+
+        @Schema(description = "field for description task", example = "Pay to bill water")
+        String description,
+
+        @Schema(description = "field for finalization task date", example = "05/07/2025")
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        LocalDate finalDate
+) {
 }
