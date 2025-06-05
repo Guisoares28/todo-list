832e4eae55f80a435c421e627b0ff32c6f5c84f4
gui.2899 <gui.2899>
2025-06-05 17:04:11 -0300

    Adicionando as tags do swagger

--- todoList/src/main/java/com/example/todoList/todoList/enums/Status.java
+++ todoList/src/main/java/com/example/todoList/todoList/enums/Status.java
@@ -1,7 +1,14 @@
 package com.example.todoList.todoList.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public enum Status {
+    @Schema(description = "Pending task", example = "PENDING")
     PENDING,
+
+    @Schema(description = "In progress task", example = "INPROGRESS")
     INPROGRESS,
+
+    @Schema(description = "Finished task", example = "FINISHED")
     FINISHED
 }
