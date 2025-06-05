77305804c2353acddcf6d4fd722c1f8a11f25ef7
gui.2899 <gui.2899>
2025-06-05 12:20:47 -0300

    Criação da nossa interface service, onde terá todos os nossos metodos CRUD

--- todoList/src/main/java/com/example/todoList/todoList/service/CrudService.java
+++ todoList/src/main/java/com/example/todoList/todoList/service/CrudService.java
@@ -1,4 +1,25 @@
 package com.example.todoList.todoList.service;
 
-public interface CrudService {
+import com.example.todoList.todoList.dto.TaskRequestDTO;
+import com.example.todoList.todoList.dto.TaskResponseDTO;
+import com.example.todoList.todoList.enums.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDate;
+
+public interface CrudService<T, ID> {
+
+    void create(TaskRequestDTO entity);
+
+    TaskResponseDTO findById(Long id);
+
+    Page<TaskResponseDTO> findAll(Pageable pageable);
+
+    Page<TaskResponseDTO> findByFinalDate(Pageable pageable, LocalDate startDate, LocalDate endDate);
+
+    Page<TaskResponseDTO> findByStatus(Pageable pageable, Status status);
+
+    Page<TaskResponseDTO> findByCurrent(Pageable pageable ,Boolean current);
+
+
 }
