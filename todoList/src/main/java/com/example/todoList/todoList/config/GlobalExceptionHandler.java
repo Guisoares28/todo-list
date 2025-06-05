312a1857625ce99da0110733be27cc73235537dd
gui.2899 <gui.2899>
2025-06-05 12:17:23 -0300

    Mudanças no retorno do status de 500 para 404

--- todoList/src/main/java/com/example/todoList/todoList/config/GlobalExceptionHandler.java
+++ todoList/src/main/java/com/example/todoList/todoList/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.todoList.todoList.config;
 
+import com.example.todoList.todoList.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,7 +13,6 @@ import java.util.List;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         List<String> errors = ex.getBindingResult()
@@ -21,6 +21,11 @@ public class GlobalExceptionHandler {
                 .map(error -> error.getField() + " : " + error.getDefaultMessage())
                 .toList();
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
 }
---
+++ todoList/src/main/java/com/example/todoList/todoList/service/CrudService.java
@@ -0,0 +1,4 @@
+package com.example.todoList.todoList.service;
+
+public interface CrudService {
+}
